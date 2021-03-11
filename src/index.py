1a197a1270da78c0f5f5d6dd7c8b65aa10e63324
luke0623 <luke0623@outlook.com>
2021-03-11 23:49:29 +0800

    update test code

--- src/index.py
+++ src/index.py
@@ -2,7 +2,6 @@ from src.scenario.admin import simple_core_path
 
 print("run all tests")
 
-
 simple_core_path.main()
 
 
--- src/scenario/admin/simple_core_path.py
+++ src/scenario/admin/simple_core_path.py
@@ -1,49 +1,25 @@
 from src.sdk.admin.admin_sdk import import_topics, import_spaces, import_pipelines
-from src.sdk.auth.auth_sdk import login
 from src.sdk.common.common_sdk import import_instances, remove_topic_collection
 from src.utils.file_utils import load_topics, load_spaces, load_pipelines, load_instances
-
-
-def __build_header():
-    pass
 
 
 def clean_instance_data():
     remove_topic_collection(["test_raw_data","test_aggregate_data","test_distinct_data"])
 
 
-    topic_data_list =[]
-
-
-
-    ## clean topic data
-
-    pass
-
-
 def import_metadata():
-    ## login with admin user
-
     import_topics(load_topics())
     import_spaces(load_spaces())
     import_pipelines(load_pipelines())
 
 
-
-
-    ## import user group
-    ## import topic
-
-
-    ## import space
-    ## import pipeline
-
 def import_instance_data():
     import_instances(load_instances())
 
 
-
 def verify_topic_data():
+
+
     pass
 
 
